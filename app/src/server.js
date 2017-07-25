174c80e189201851b093a29cdf9395eeb115c679
Srimoyee <srimoyeebhattacharyya@gmail.com>
2017-07-25 22:59:31 +0530

    Added stylesheet serving routes

--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 //your routes here
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/ui', 'index.html'));
+});
+
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
 });
 
 app.listen(80, function () {
