4b4d0edb5c1e769a3212998d29e36fac0cf4cf9b
Srimoyee <srimoyeebhattacharyya@gmail.com>
2017-08-18 01:28:52 +0530

    Fixed advertise form input fields, radio buttons, mobile view issues

--- app/src/ui/script.js
+++ app/src/ui/script.js
@@ -217,7 +217,7 @@ function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if(slides){
+    if(slides.length>0){
         for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
         }
@@ -226,8 +226,8 @@ function showSlides() {
         for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
         }
-        slides[slideIndex-1].style.display = "block";
+        slides[slideIndex-1].style.display = 'block';
         dots[slideIndex-1].className += " active";
         setTimeout(showSlides, 5000); // Change image every 10 seconds
-        }
+    }
 }
